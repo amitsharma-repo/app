f02d247ed9f2425aca864adab8172a9fd5f7e019
ultimatepandit <ultimatepandit@LAPTOP-787BT108.localdomain>
2020-07-18 06:59:35 +0530

    max_index implemetation corretion

--- include/maximum_index.h
+++ include/maximum_index.h
@@ -1,3 +1,4 @@
 #include<vector>
+//Given an array A[] of N positive integers. The task is to find the maximum of j - i subjected to the constraint of A[i] <= A[j].
 int find_max_index(std::vector<int> vec_input);
 
--- src/geeks_solver/maximum_index.cpp
+++ src/geeks_solver/maximum_index.cpp
@@ -1,4 +1,5 @@
 #include<maximum_index.h>
+#include<iostream>
 int find_max_index(std::vector<int> vec_input)
 {
     int n_start_idx = 0;
@@ -11,12 +12,14 @@ int find_max_index(std::vector<int> vec_input)
             break;
         n_end_idx--;
     }
+    n_start_idx = n_start_idx + 1;
     result = n_end_idx;
     while(n_start_idx + result < vec_input.size()) {
         n_end_idx = n_start_idx + result;
-        for( int idx = vec_input.size(); idx > n_end_idx; --idx) {
-            if( vec_input[n_end_idx] >= vec_input[n_start_idx] ) {
-                result = n_end_idx = n_start_idx;
+        for( int idx = vec_input.size() -1; idx > n_end_idx; --idx) {
+            if( vec_input[idx] >= vec_input[n_start_idx] ) {
+                if( result < idx - n_start_idx )
+                    result = idx - n_start_idx;
                 break;
             }
                 
